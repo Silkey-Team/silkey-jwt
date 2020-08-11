0f382f077496c43634977486d82f50ad1c120d6b
DZariusz <dzariusz@gmail.com>
2020-08-11 20:58:15 +0200

    feat(*): generate and validate silkey compatible jwt

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import {silkeyJwtGenerator} from "./SilkeyJwtGenerator.js";
+import {silkeyJwtPayloadVerificator} from "./SilkeyJwtPayloadVerificator.js";
+export { silkeyJwtGenerator, silkeyJwtPayloadVerificator };
